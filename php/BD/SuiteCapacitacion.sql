e4599e57ad3423cdb05cfacd162cf9e5819aaac4
yankee2311 <jeansaenz0906@gmail.com>
2023-01-11 14:35:56 -0600

    0.2
    Se trabaja en modificar los archivo en función a los requerimientos de las bases de datos actualizadas

--- php/BD/SuiteCapacitacion.sql
+++ php/BD/SuiteCapacitacion.sql
@@ -1,4 +1,4 @@
---Crear una base de datos, en esta app la base de datos se llama SuiteCapacitacion
+-Crear una base de datos, en esta app la base de datos se llama SuiteCapacitacion
 
 --Crea la tabla para los usuarios
 CREATE TABLE Usuarios (
@@ -25,12 +25,22 @@ CREATE TABLE Empleados (
 CREATE TABLE Cursos (
    Id INTEGER PRIMARY KEY,
    Nom_curso VARCHAR(50) NOT NULL,
+   Objetivo VARCHAR(500) NOT NULL,
    Area VARCHAR(50) NOT NULL,
    Duracion INTEGER NOT NULL,
-   Tipo VARCHAR(50) NOT NULL,
-   Modalidad VARCHAR(50) NOT NULL,
+   Capacitador VARCHAR(50) NOT NULL, --INTERNO O EXTERNO
+   Modalidad VARCHAR(50) NOT NULL, --ONLINE, PRESENCIAL O HIBRIDO
    Activo BOOLEAN NOT NULL
 );
+
+--Crea los cursos programados del mes.
+CREATE TABLE programados (
+   Id INTEGER PRIMARY KEY,
+   Id_Curso INTEGER NOT NULL,
+   Mes VARCHAR(50) NOT NULL,
+   Sucursal VARCHAR(50) NOT NULL
+   Tipo VARCHAR (50) NOT NULL --Programado, Inducción o Ambos
+   )
 
 --Crea la tabla Asistencia
 CREATE TABLE Asistencia (
