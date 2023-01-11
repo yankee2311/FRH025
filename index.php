836dbb4d4c0eb4d54634be79165c5158bbeb26eb
yankee2311 <jeansaenz0906@gmail.com>
2023-01-11 17:49:44 -0600

    0.2.1
    se modifica el login, es funcional 100% no contiene errores, pendiente añadir cerrar sesión

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
   if (isset($_SESSION['username'])) {
     // La sesión está iniciada, cargar la página
     // (el resto del código de la página iría aquí)
+    //coloco en pantalla el nombre de usuario 
+    echo $_SESSION['username'];
   } else {
     // La sesión no está iniciada, redirigir al usuario a login.php
     header('Location: login.php');
   }
 ?>
+
